43afdaab75dcbd71f7a0cd53dcb5ffda3acddbae
godwinsodogandji <jeansodogandji@gmail.com>
2024-11-06 11:19:54 +0100

    init

---
+++ port_folio_front/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-4xl font-bold">Bienvenue sur mon Portfolio !</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
